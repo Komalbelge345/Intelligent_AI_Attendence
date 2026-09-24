aa009e684cab25c809b1d507758664e99c8e1802
Komal Bekge <shridevi929292@gmail.com>
2026-09-25 00:07:04 +0530

    student registration through voice and face recognition

--- src/pipelines/face_pipeline.py
+++ src/pipelines/face_pipeline.py
@@ -67,7 +67,7 @@ def get_trained_model():
     return {'clf':clf,'X':X,'y':y}
 
 def train_classifier():
-    st.catche_resource.clear()
+    st.cache_resource.clear()
     model_data = get_trained_model()
     return bool(model_data)
 
